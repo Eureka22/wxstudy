45296c86717604424f64f265c84658fbab18b6ba
xh <3524130451@qq.com>
2022-05-18 12:22:52 +0800

    weeding
    weeding

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,41 @@
 // index.js
-Page({})
+Page({
+  data:{
+    isPlayingMusic:false
+  },
+  bgm:null,
+  music_url:"https://link.hhtjim.com/163/28403111.mp3",
+  music_coverImageUrl:"/images/bg_1.png",
+  onReady: function () {
+    this.bgm = wx.getBackgroundAudioManager()
+    this.bgm.title='merry me',
+    this.bgm.epname='weeding',
+    this.bgm.singer='singer',
+    this.bgm.coverImgUrl=this.music_coverImageUrl
+    this.bgm.onCanplay(()=> {
+      this.bgm.pause()
+    })
+    this.bgm.src = this.music_url
+  },
+  play:function(e){
+    if(this.data.isPlayingMusic){
+      this.bgm.pause()
+    }else{
+      this.bgm.play()
+    }
+    this.setData({
+      isPlayingMusic: !this.data.isPlayingMusic
+    })
+  },
+  // 一键拨打电话
+  callPhone:function(){
+    wx.makePhoneCall({
+      phoneNumber: '123456789',
+    })
+  },
+  callBridge:function(){
+    wx.makePhoneCall({
+      phoneNumber: '1357890096',
+    })
+  }
+})
